0c15dc06d59a4499c22ee89747c841264efbb28d
Armen <manukyanarm@yahoo.com>
2017-05-23 21:02:22 +0400

    commiting

---
+++ src/main/java/com/mywebapp/hello/HelloServlet.java
@@ -0,0 +1,12 @@
+package com.mywebapp.hello;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+            throws IOException {
+        httpServletResponse.getWriter().print("Hello from HelloServlet");
+    }
+}
